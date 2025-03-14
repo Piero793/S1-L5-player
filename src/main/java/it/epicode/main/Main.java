04e7dd2e0d6300829479efff582be110d83e9902
“Piero793” <pierolacitignola7@gmail.com>
2025-03-14 14:10:56 +0100

    Modifiche sul Main

--- src/main/java/it/epicode/main/Main.java
+++ src/main/java/it/epicode/main/Main.java
@@ -18,30 +18,22 @@ public class Main {
         Immagine immagine = new Immagine("Logo Epicode", 7);
         Multimedia video2 = new Video("Java tutorial", 100, 10, 8);
 
-        Multimedia[] arrayMultimedia = {video, audio,video2};
+        Multimedia[] arrayMultimedia = {video, audio, video2};
 
         int scelta = -1;
 
-        while (scelta != 0) {
-            System.out.println("Scegli un'opzione:");
-            System.out.println("1. Avvia un elemento specifico");
-            System.out.println("2. Aumenta luminosità");
-            System.out.println("3. Diminuisci luminosità");
-            System.out.println("4. Alza volume");
-            System.out.println("5. Abbassa volume");
-            System.out.println("0. Esci");
-            scelta = input.nextInt();
-
-            if (scelta == 0) {
-                System.out.println("Arrivederci!");
-                break;
-            }
-
+        while (true) {
             System.out.println("Seleziona l'elemento (da 0 a " + (arrayMultimedia.length - 1) + "):");
             for (int i = 0; i < arrayMultimedia.length; i++) {
                 System.out.println(i + ": " + arrayMultimedia[i].toString());
             }
+            System.out.println("0: Esci dal programma");
             int indice = input.nextInt();
+
+            if (indice == 0) {
+                System.out.println("Arrivederci!");
+                break;
+            }
 
             if (indice < 0 || indice >= arrayMultimedia.length) {
                 System.out.println("Indice non valido!");
@@ -50,43 +42,57 @@ public class Main {
 
             Multimedia elemento = arrayMultimedia[indice];
 
-            switch (scelta) {
-                case 1:
-                    elemento.play();
-                    break;
-                case 2:
-                    if (elemento instanceof Luminosità) {
-                        ((Luminosità) elemento).aumentaLuminosita();
-                    } else {
-                        System.out.println("Nessun controllo sulla luminosità.");
-                    }
-                    break;
-                case 3:
-                    if (elemento instanceof Luminosità) {
-                        ((Luminosità) elemento).diminuisciLuminosita();
-                    } else {
-                        System.out.println("Nessun controllo sulla luminosità.");
-                    }
-                    break;
-                case 4:
-                    if (elemento instanceof Volume) {
-                        ((Volume) elemento).alzaVolume();
-                    } else {
-                        System.out.println("Nessun controllo sul volume.");
-                    }
-                    break;
-                case 5:
-                    if (elemento instanceof Volume) {
-                        ((Volume) elemento).abbassaVolume();
-                    } else {
-                        System.out.println("Nessun controllo sul volume.");
-                    }
-                    break;
-                default:
-                    System.out.println("Scelta non valida!");
-                    break;
+            while (true) {
+                System.out.println("Scegli un'opzione per l'elemento selezionato:");
+                System.out.println("1. Aumenta luminosità");
+                System.out.println("2. Diminuisci luminosità");
+                System.out.println("3. Alza volume");
+                System.out.println("4. Abbassa volume");
+                System.out.println("5. Avvia l'elemento con le modifiche");
+                System.out.println("0. Esci dal programma");
+                int subScelta = input.nextInt();
+
+                switch (subScelta) {
+                    case 1:
+                        if (elemento instanceof Luminosità) {
+                            ((Luminosità) elemento).aumentaLuminosita();
+                        } else {
+                            System.out.println("Nessun controllo sulla luminosità.");
+                        }
+                        break;
+                    case 2:
+                        if (elemento instanceof Luminosità) {
+                            ((Luminosità) elemento).diminuisciLuminosita();
+                        } else {
+                            System.out.println("Nessun controllo sulla luminosità.");
+                        }
+                        break;
+                    case 3:
+                        if (elemento instanceof Volume) {
+                            ((Volume) elemento).alzaVolume();
+                        } else {
+                            System.out.println("Nessun controllo sul volume.");
+                        }
+                        break;
+                    case 4:
+                        if (elemento instanceof Volume) {
+                            ((Volume) elemento).abbassaVolume();
+                        } else {
+                            System.out.println("Nessun controllo sul volume.");
+                        }
+                        break;
+                    case 5:
+                        elemento.play();
+                        break;
+                    case 0:
+                        System.out.println("Arrivederci!");
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("Scelta non valida!");
+                        break;
+                }
             }
         }
-
     }
 }
